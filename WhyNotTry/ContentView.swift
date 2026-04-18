cb82c6ffa2678014f94e2f236c7d4c19dfec48e5
Amine Ramadane <amineram.work@gmail.com>
2026-04-18 19:11:30 +0100

    Initial Commit

---
+++ WhyNotTry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhyNotTry
+//
+//  Created by Amine Ramadane on 18/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Amine Ramadane on 18/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
